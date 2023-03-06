ee64fff65e6df9ba81cec3b518e11f9a98d093a5
七画一只妖 <1157529280@qq.com>
2023-03-06 09:23:33 +0800

    多线程

---
+++ src/Basic/Basic1.java
@@ -0,0 +1,17 @@
+package Basic;
+
+public class Basic1 {
+    public static void main(String[] args) {
+//        String a = "123";
+//        String b = "123";
+//        String b = new String("123");
+
+//        Integer a = 1;
+//        int b = 2;
+        Integer b = 1;
+        int a = 1;
+//        int b = 1;
+
+        System.out.println(a==b);
+    }
+}
