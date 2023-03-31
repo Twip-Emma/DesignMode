4e2334aecb85031ddc2e730e3a8e02389be9471b
七画一只妖 <1157529280@qq.com>
2023-03-31 10:23:57 +0800

    lambda分组聚合

--- src/lambda/test/LambdaTest1.java
+++ src/lambda/test/LambdaTest1.java
@@ -2,22 +2,24 @@ package lambda.test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class LambdaTest1 {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
-        users.add(new User("tom", 8));
+        users.add(new User("tom", 9));
         users.add(new User("mike", 9));
-        users.add(new User("jack", 11));
-        users.add(new User("alice", 15));
+        users.add(new User("alice", 11));
+        users.add(new User("alice", 11));
 
-        AtomicReference<Integer> bigMan = new AtomicReference<>(0);
-        users.forEach(user -> {
-            if(user.getAge() > 10) {
-                bigMan.getAndSet(bigMan.get() + 1);
-            }
-        });
-        System.out.println(bigMan.get());
+        Map<Integer, Long> userInfo = users.stream().
+                collect(Collectors.groupingBy(User::getAge, Collectors.counting()));
+
+        System.out.println("11岁的有" + userInfo.get(11) + "位");
+
+
     }
 }
