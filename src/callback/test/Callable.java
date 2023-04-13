f2d8cf2d3f99b44183f55666d256159e90bb72ab
七画一只妖 <1157529280@qq.com>
2023-04-13 09:11:11 +0800

    回调函数测试（多线程）

--- src/callback/test/Callable.java
+++ src/callback/test/Callable.java
@@ -10,6 +10,7 @@ public interface Callable {
 
 /**
  * 定义类Mother实现了Callable接口，实现了call()回调函数的具体内容
+ * (同步)
  */
 class Mother implements Callable {
     Son son;
@@ -29,6 +30,34 @@ class Mother implements Callable {
         System.out.println("儿子到学校了");
     }
 }
+
+
+/**
+ * 定义类Mother实现了Callable接口，实现了call()回调函数的具体内容
+ * (异步)
+ */
+class Mother2 implements Callable {
+
+    Son son;
+
+    public Mother2(Son son)
+    {
+        this.son=son;
+    }
+
+    public void parting()
+    {
+        System.out.println("开始执行异步回调函数");
+        new Thread(() -> son.rideTrain(Mother2.this)).start();
+        System.out.println("异步回调函数执行完成");//开启线程处理儿子坐火车的函数，这条打印语句会在回调函数执行完成前执行。
+    }
+
+    @Override
+    public void call() {
+        System.out.println("儿子到学校了");
+    }
+}
+
 
 
 /**
@@ -53,8 +82,10 @@ class Son {
  */
 class Test {
     public static void main(String[] args) {
+        // Mother1 同步，Mother2 异步
+
         Son jack=new Son();
-        Mother mother=new Mother(jack);
+        Mother2 mother=new Mother2(jack);
         mother.parting();
     }
 }
